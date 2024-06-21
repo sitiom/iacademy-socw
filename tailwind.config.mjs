1437304a93905d6712eb55f9603d1d1e331eee40
Ryan Caezar Itang <sitiom@proton.me>
2024-06-21 15:57:32 +0800

    feat: add proper partners list

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,12 +13,17 @@ export default {
     extend: {
       animation: {
         'infinite-scroll': 'infinite-scroll 25s linear infinite',
+        'infinite-scroll-reverse': 'infinite-scroll-reverse 25s linear infinite',
       },
       keyframes: {
         'infinite-scroll': {
           from: { transform: 'translateX(0)' },
           to: { transform: 'translateX(-100%)' },
-        }
+        },
+        'infinite-scroll-reverse': {
+          from: { transform: 'translateX(-100%)' },
+          to: { transform: 'translateX(0)' },
+        },
       },
       fontFamily: {
         sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
